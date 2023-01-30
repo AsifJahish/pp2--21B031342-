00bf08006789ee37d39bf7439ccbc4e21543577c
asifjahish1@gmail.com <asifjahish1@gmail.com>
2023-01-30 09:47:13 +0600

    for

---
+++ tsis2/forLoop/for2.py
@@ -0,0 +1,11 @@
+
+
+language = "my name is ......"
+for letter in language:
+    print(letter, end=" ")
+    
+print("\n")
+for i in range(len(language)):
+    print(language[i], end=" ")
+
+print("\n")
---
+++ tsis2/forLoop/for3.py
@@ -0,0 +1,20 @@
+
+
+
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+for key in person:
+    print(key, end=" ")
+print ("\n")
+for key, value in person.items():
+    print(key, value) 
