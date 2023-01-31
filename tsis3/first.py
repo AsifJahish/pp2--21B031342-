b9e88e9fb28fdf4242100a08a1f4a42a5bcddf24
asifjahish1@gmail.com <asifjahish1@gmail.com>
2023-01-31 19:03:34 +0600

    Tsis 3

---
+++ tsis3/first.py
@@ -0,0 +1,31 @@
+
+
+class Person():
+    
+    
+    
+    def printString(self, first_name, last_name, age, address):
+        self.first_name= first_name
+        self.last_name= last_name
+        self.age= age;
+        self.address= address
+    
+    def printString(self):
+        return '{}{}{}{}'.format(self.first_name,self.last_name, self.age, self.address)
+    
+    def toUpper(self):
+        self.printString
+        print (self. printString().upper())
+
+person1= Person()
+person1.first_name=input(str("enter the first name of the person  "))
+person1.last_name= input(str("enter the last name of the person  "))
+person1.age= input(str("enter the age of the person  "))
+person1.address= input(str("enter the address of the perso  "))
+
+print(person1.first_name)
+print(person1.last_name)
+print(person1.age)
+print(person1.address)
+
+person1.toUpper()
