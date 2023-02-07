9caf4086c729ad452b9bb5a5e54b91578181756d
asifjahish1@gmail.com <asifjahish1@gmail.com>
2023-02-07 23:52:43 +0600

    function

---
+++ tsis3/function1/task1.py
@@ -0,0 +1,10 @@
+
+
+
+def grams_to_ounces(grams):
+    ounces = 28.3495231 * grams
+    return ounces
+
+grams= int(input("enter the amount of grams   "))
+
+print(grams_to_ounces(grams))
---
+++ tsis3/function1/task3.py
@@ -0,0 +1,10 @@
+
+
+
+def solve(numheads, numlegs):
+    for rabbits in range(numheads + 1):
+        chickens = numheads - rabbits
+        if (2 * chickens + 4 * rabbits == numlegs):
+            return chickens, rabbits
+    return None, None
+numheads = int(input(""))
