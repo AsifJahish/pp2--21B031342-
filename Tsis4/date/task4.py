b1e91bb8300c71eae1eb8c40cdf86ffcbe46ca96
AsifJahish <asifjahish1@gmail.com>
2023-02-17 23:54:26 +0600

    tsis4

--- Tsis4/date/task4.py
+++ Tsis4/date/task4.py
@@ -3,15 +3,17 @@
 #  Write a Python program to calculate two date difference in seconds.
 
 from datetime import datetime
-def difference_date(first, second):
-    return (first-second).total_seconds()
 
+# get the two dates as strings from the user
+date1 = input("Enter the first date (YYYY-MM-DD HH:MM:SS): ")
+date2 = input("Enter the second date (YYYY-MM-DD HH:MM:SS): ")
 
-date_input= input("Enter datetime in format 'YYYY-MM-DD HH:MM:SS':")
-b=datetime.strptime(date_input, '%Y-%m-%d %H:%M:%S')
+# convert the strings to datetime objects
+date1_obj = datetime.strptime(date1, '%Y-%m-%d %H:%M:%S')
+date2_obj = datetime.strptime(date2, '%Y-%m-%d %H:%M:%S')
 
-date_input2= input("Enter the seond datetime in format 'YYYY-MM-DD HH:MM:SS':")
-c=datetime.strptime(date_input, '%Y-%m-%d %H:%M:%S')
+# calculate the difference between the two dates in seconds
+diff_seconds = abs((date2_obj - date1_obj).total_seconds())
 
-d= difference_date(b ,c)
-print(d)
+# print the result
+print("The difference between the two dates is {} seconds".format(diff_seconds))
