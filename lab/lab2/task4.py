5b10330433043f4f4007c2de21cbdcd2ffd512c5
AsifJahish <asifjahish1@gmail.com>
2023-02-27 22:35:52 +0600

    tsis5

---
+++ lab/lab2/task4.py
@@ -0,0 +1,12 @@
+
+
+
+class Clothing:
+    def __init__(self, name, size, color, price):
+        self.name=name
+        self.size=size
+        self.color=color
+        self.price=price
+    def display_info(self):
+        return f"name is : {self.name}, size of the color"
+        
